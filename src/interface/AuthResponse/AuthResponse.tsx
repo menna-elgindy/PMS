880d6a1d86a7a3160ae3de5c30e5f5fd34b2e260
menna alaa <mennaelgindy98@gmail.com>
2024-12-01 13:44:46 +0200

    auth-interfaces

--- src/interface/AuthResponse/AuthResponse.tsx
+++ src/interface/AuthResponse/AuthResponse.tsx
@@ -1,41 +1,45 @@
 export interface AxiosErrorResponse {
-    data: {
-      message: string;
-    };
-    [key: string]: any;
-  }
-  
-  export interface LoginFormData {
-    email: string;
-    password: string;
-  }
-  
-  export interface RegisterFormData extends LoginFormData {
-    userName: string;
-    country: string;
-    phoneNumber: string;
-    confirmPassword: string;
-    profileImage:any
-  }
-  
-  export interface ForgetPasswordFormData {
-    email: string;
-  }
-  
-  export interface ResetPasswordFormData
-    extends LoginFormData,
-      Pick<RegisterFormData, "confirmPassword"> {
-    seed: string; 
-  }
+  data: {
+    message: string;
+  };
+  [key: string]: any;
+}
 
-  export interface VerifyRegister {
-    email : string,
-    code:string
-  }
-   
-  export interface AuthTitleProps {
-    welcomeText: string;
-    title: string;
-    firstLetter: string; 
-  }
-  
+export interface LoginFormData {
+  email: string;
+  password: string;
+}
+
+export interface RegisterFormData extends LoginFormData {
+  userName: string;
+  country: string;
+  phoneNumber: string;
+  confirmPassword: string;
+  profileImage: any;
+}
+
+export interface ForgetPasswordFormData {
+  email: string;
+}
+
+export interface ResetPasswordFormData
+  extends LoginFormData,
+    Pick<RegisterFormData, "confirmPassword"> {
+  seed: string;
+}
+
+export interface VerifyRegister {
+  email: string;
+  code: string;
+}
+
+export interface AuthTitleProps {
+  welcomeText: string;
+  title: string;
+  firstLetter: string;
+}
+export interface ChangePasswordPayload {
+  oldPassword: string;
+  newPassword: string;
+  confirmNewPassword: string;
+}
