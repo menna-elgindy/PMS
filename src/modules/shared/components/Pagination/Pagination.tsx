eb4b83e4cf20ae94ff986a1766bf970d081d6fd5
Your Name <ranamostafa2210@gmail.com>
2024-12-03 21:56:12 +0200

    add-filter-with-pagination for users and projects

--- src/modules/shared/components/Pagination/Pagination.tsx
+++ src/modules/shared/components/Pagination/Pagination.tsx
@@ -1,99 +1,103 @@
-import Dropdown from 'react-bootstrap/Dropdown';
+import Dropdown from "react-bootstrap/Dropdown";
 
 interface paginationInfo {
-    totalNumberOfPages:number[],
-    currentPage : number|string,
-    numberOfPages : number|string,
-    paginatedListFunction: (UsersFilterOptions:{pageNumber:number,pageSize:number})=>void ,
-    pageNumber:number,
-    numOfRecords:number
-
-  }
-  
-
-
-
-const Pagination:React.FC<paginationInfo> = ({totalNumberOfPages,paginatedListFunction,pageNumber,numOfRecords}) => {
-
-
-const nextBtn =(pageSize:number,pageNumber:number)=>{
-
-  if(pageNumber <= totalNumberOfPages.length) {
-    paginatedListFunction({pageNumber:pageNumber,pageSize:pageSize})
-
-    
-  }
-
-
-}
-const prevBtn =(pageSize:number,pageNumber:number)=>{
-
-  console.log(pageNumber);
-  if(pageNumber >=1){
-    paginatedListFunction({pageNumber:pageNumber,pageSize:pageSize})
-
-  }
- 
+  totalNumberOfPages: number[];
+  currentPage?: number | string;
+  numberOfPages?: number | string;
+  paginatedListFunction: (UsersFilterOptions: {
+    pageNumber: number;
+    pageSize: number;
+  }) => void;
+  pageNumber: number;
+  numOfRecords: number;
 }
 
+const Pagination: React.FC<paginationInfo> = ({
+  totalNumberOfPages,
+  paginatedListFunction,
+  pageNumber,
+  numOfRecords,
+}) => {
+  const nextBtn = (pageSize: number, pageNumber: number) => {
+    if (pageNumber <= totalNumberOfPages.length) {
+      paginatedListFunction({ pageNumber: pageNumber, pageSize: pageSize });
+    }
+  };
+  const prevBtn = (pageSize: number, pageNumber: number) => {
+    console.log(pageNumber);
+    if (pageNumber >= 1) {
+      paginatedListFunction({ pageNumber: pageNumber, pageSize: pageSize });
+    }
+  };
 
- return <>
-    <div className="bg-white py-3">
-         
- <nav aria-label="Page navigation example">
-  <ul className="pagination  align-items-center gap-3 justify-content-end">
+  return (
+    <>
+      <div className="bg-white py-3">
+        <nav aria-label="Page navigation example">
+          <ul className="pagination  align-items-center gap-3 justify-content-end">
+            <span className="">showing</span>
+            <Dropdown>
+              <Dropdown.Toggle
+                variant="transparent"
+                className="paginationDropDownStyle"
+                id="dropdown-basic"
+              >
+                {pageNumber} <i className="fa-solid fa-chevron-down"></i>
+              </Dropdown.Toggle>
 
-    <span className=''>
-        showing
-    </span>
-    <Dropdown>
-      <Dropdown.Toggle variant='transparent' className='paginationDropDownStyle'  id="dropdown-basic">
-       {pageNumber} <i className="fa-solid fa-chevron-down"></i>
-        
+              <Dropdown.Menu>
+                {totalNumberOfPages?.map((page) => (
+                  <Dropdown.Item
+                    key={page}
+                    onClick={() =>
+                      paginatedListFunction({ pageNumber: page, pageSize: 5 })
+                    }
+                  >
+                    {page}
+                  </Dropdown.Item>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
 
-      </Dropdown.Toggle>
+            <span> of {numOfRecords} Results</span>
 
-      <Dropdown.Menu>
+            <div>
+              <span>Page {pageNumber}</span>
+              <span className="mx-2">of {totalNumberOfPages?.length}</span>
+            </div>
 
-        {
-          totalNumberOfPages?.map((page)=>       <Dropdown.Item key={page}  onClick={()=>paginatedListFunction({pageNumber:page,pageSize:5})} > 
+            <li className="page-item">
+              <a
+                className={
+                  pageNumber == 1
+                    ? "page-link pagniationLink disabledArrow"
+                    : "page-link pagniationLink "
+                }
+                onClick={() => prevBtn(5, pageNumber - 1)}
+                aria-label="Previous"
+              >
+                <i className="fa-solid fa-chevron-left"></i>
+              </a>
+            </li>
 
-          {page}
-              
-              </Dropdown.Item>
-        )
-        }
- 
+            <li className="page-item">
+              <a
+                className={
+                  pageNumber == totalNumberOfPages.length
+                    ? "page-link pagniationLink disabledArrow"
+                    : "page-link pagniationLink "
+                }
+                onClick={() => nextBtn(5, pageNumber + 1)}
+                aria-label="Next"
+              >
+                <i className="fa-solid fa-chevron-right "></i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
 
-      </Dropdown.Menu>
-    </Dropdown>
-
-    <span> of {numOfRecords} Results</span>
-
-        <div>
-            <span>Page {pageNumber}</span>
-            <span className='mx-2'>of {totalNumberOfPages?.length}</span>
-        </div>
-
-    <li className="page-item">
-      <a className={pageNumber == 1 ? "page-link pagniationLink disabledArrow" : "page-link pagniationLink "} onClick={()=>prevBtn(5,pageNumber-1)}   aria-label="Previous">
-      <i className="fa-solid fa-chevron-left"></i>
-      </a>
-    </li>
-    
-    <li className="page-item">
-      <a className={pageNumber == totalNumberOfPages.length ? "page-link pagniationLink disabledArrow" : "page-link pagniationLink "}  onClick={()=>nextBtn (5,pageNumber+1)
-      } aria-label="Next">
-       
-       
-      <i className="fa-solid fa-chevron-right " ></i>
-      </a>
-    </li>
-  </ul>
-</nav>
-    </div>
- 
- </>
-}
-
-export default Pagination
+export default Pagination;
--- src/modules/users/components/UsersList/UsersList.tsx
+++ src/modules/users/components/UsersList/UsersList.tsx
@@ -18,27 +18,17 @@ import useFetch from "../../../../hooks/useFetch";
 import Pagination from "../../../shared/components/Pagination/Pagination";
 
 const UsersList = () => {
-
-
-
-
-
-
-   const[pageNum,setPageNum]= useSearchParams()
-
-
-
+  const [pageNum, setPageNum] = useSearchParams();
 
   const [usersList, setUsersList] = useState<UsersListResponse[]>([]);
   const [arrayOfPages, setArrayOfPages] = useState<number[]>([]);
   const [numOfRecords, setNumOfRecords] = useState<number>(0);
-  const [totalNumberOfPages, setTotalNumberOfPages] = useState<number>([]);
+  const [totalNumberOfPages, setTotalNumberOfPages] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
   const [counterLoading, setCounterLoadind] = useState<number>(0);
   const [searchParams] = useSearchParams();
 
   // Function to fetch the list of users from the API
-
   const getAllUsers = async (params: UsersFilterOptions | null = null) => {
     if (counterLoading == 0) {
       setLoading(true);
@@ -60,16 +50,14 @@ const UsersList = () => {
       );
       setArrayOfPages(
         Array(response.data.totalNumberOfPages)
-          .fill()
+          .fill(0)
           .map((_, i) => i + 1)
       );
       setNumOfRecords(response?.data?.totalNumberOfRecords);
       setUsersList(response.data?.data);
-      setTotalNumberOfPages(response?.data?.totalNumberOfPages)
-      setPageNum({pageNum:response?.data?.pageNumber})
-
-
-      
+      setTotalNumberOfPages(response?.data?.totalNumberOfPages);
+      setPageNum({ pageNum: Number(response?.data?.pageNumber).toString() });
+      // setPageNum({ pageNum: response?.data?.pageNumber?.toString() });
     } catch (error) {
       const axiosError = error as AxiosError<AxiosErrorResponse>;
       toast.error(axiosError.response?.data.message);
@@ -83,7 +71,7 @@ const UsersList = () => {
       {
         params: {
           pageSize: 5,
-          pageNumber: Number(pageNum.get('pageNum')),
+          pageNumber: Number(searchParams.get("pageNum")),
           userName: searchParams.get("name") || null,
           email: searchParams.get("email") || null,
           country: searchParams.get("country") || null,
@@ -121,8 +109,7 @@ const UsersList = () => {
   };
 
   useEffect(() => {
-    getAllUsers({pageNumber:pageNum.get('pageNum')})
-    
+    getAllUsers({ pageNumber: Number(pageNum.get("pageNum")) });
   }, []);
   const usersListToDisplay =
     filteredUsers !== null && !usersLoading && filteredUsers
@@ -142,96 +129,100 @@ const UsersList = () => {
         ) : (
           <div className="table-responsive p-5">
             <Filtration pageName="users" />
-            {usersListToDisplay.length > 0 ? (
+            {usersListToDisplay!.length > 0 ? (
               <>
-                            <table className="table table-striped table-borderless ">
-                <thead className="table-dark">
-                  <tr>
-                    <th className="table-header" scope="col">
-                      User Name
-                    </th>
-                    <th className="table-header" scope="col">
-                      Status
-                    </th>
-                    <th className="table-header" scope="col">
-                      Phone Number
-                    </th>
-                    <th className="table-header" scope="col">
-                      Email
-                    </th>
-                    <th className="table-header" scope="col">
-                      Date Created
-                    </th>
-                    <th className="table-header" scope="col">
-                      Actions
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {usersListToDisplay.map((user) => (
-                    <tr key={user.id}>
-                      <td className="table-data">{user.userName}</td>
-                      <td className="table-data">
-                        {user.isActivated ? (
-                          <span className="badge bg-success">Active</span>
-                        ) : (
-                          <span className="badge bg-danger">Non-Active</span>
-                        )}
-                      </td>
-                      <td className="table-data">{user.phoneNumber}</td>
-                      <td
-                        className="table-data text-truncate"
-                        style={{ maxWidth: "150px" }}
-                      >
-                        {user.email}
-                      </td>
-                      <td className="table-data">
-                        {formatDate(user.creationDate)}
-                      </td>
-                      <td className="table-data">
-                        <div className="dropdown">
-                          <button
-                            className="btn btn-sm btn-light border dropdown-toggle"
-                            type="button"
-                            id={`dropdownMenuButton-${user.id}`}
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"
-                          >
-                            <i className="fa-solid fa-ellipsis"></i>
-                          </button>
-                          <ul
-                            className="dropdown-menu"
-                            aria-labelledby={`dropdownMenuButton-${user.id}`}
-                          >
-                            <li>
-                              <button
-                                className="dropdown-item text-primary"
-                                onClick={() => toggleActivation(+user.id)}
-                              >
-                                <i className="fa fa-toggle-off mx-2"></i>
-                                {user.isActivated ? "Deactivate" : "Activate"}
-                              </button>
-                            </li>
-                            <li>
-                              <Link
-                                to=""
-                                state={{ type: "edit" }}
-                                className="dropdown-item text-success"
-                              >
-                                <i className="fa fa-eye mx-2"></i>
-                                View
-                              </Link>
-                            </li>
-                          </ul>
-                        </div>
-                      </td>
+                <table className="table table-striped table-borderless ">
+                  <thead className="table-dark">
+                    <tr>
+                      <th className="table-header" scope="col">
+                        User Name
+                      </th>
+                      <th className="table-header" scope="col">
+                        Status
+                      </th>
+                      <th className="table-header" scope="col">
+                        Phone Number
+                      </th>
+                      <th className="table-header" scope="col">
+                        Email
+                      </th>
+                      <th className="table-header" scope="col">
+                        Date Created
+                      </th>
+                      <th className="table-header" scope="col">
+                        Actions
+                      </th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-                    <Pagination pageNumber={Number(pageNum.get('pageNum'))} numOfRecords={numOfRecords} totalNumberOfPages={arrayOfPages} paginatedListFunction={getAllUsers}/>
+                  </thead>
+                  <tbody>
+                    {usersListToDisplay!.map((user) => (
+                      <tr key={user.id}>
+                        <td className="table-data">{user.userName}</td>
+                        <td className="table-data">
+                          {user.isActivated ? (
+                            <span className="badge bg-success">Active</span>
+                          ) : (
+                            <span className="badge bg-danger">Non-Active</span>
+                          )}
+                        </td>
+                        <td className="table-data">{user.phoneNumber}</td>
+                        <td
+                          className="table-data text-truncate"
+                          style={{ maxWidth: "150px" }}
+                        >
+                          {user.email}
+                        </td>
+                        <td className="table-data">
+                          {formatDate(user.creationDate)}
+                        </td>
+                        <td className="table-data">
+                          <div className="dropdown">
+                            <button
+                              className="btn btn-sm btn-light border dropdown-toggle"
+                              type="button"
+                              id={`dropdownMenuButton-${user.id}`}
+                              data-bs-toggle="dropdown"
+                              aria-expanded="false"
+                            >
+                              <i className="fa-solid fa-ellipsis"></i>
+                            </button>
+                            <ul
+                              className="dropdown-menu"
+                              aria-labelledby={`dropdownMenuButton-${user.id}`}
+                            >
+                              <li>
+                                <button
+                                  className="dropdown-item text-primary"
+                                  onClick={() => toggleActivation(+user.id)}
+                                >
+                                  <i className="fa fa-toggle-off mx-2"></i>
+                                  {user.isActivated ? "Deactivate" : "Activate"}
+                                </button>
+                              </li>
+                              <li>
+                                <Link
+                                  to=""
+                                  state={{ type: "edit" }}
+                                  className="dropdown-item text-success"
+                                >
+                                  <i className="fa fa-eye mx-2"></i>
+                                  View
+                                </Link>
+                              </li>
+                            </ul>
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <Pagination
+                  pageNumber={Number(pageNum.get("pageNum"))}
+                  numOfRecords={numOfRecords}
+                  totalNumberOfPages={arrayOfPages}
+                  paginatedListFunction={getAllUsers}
+                />
               </>
-
             ) : (
               <NoData />
             )}
