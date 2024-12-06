43f3569a59a1632920689f6d6009876b7abb2a79
sarah hesham <sarah.h.nashaat@gmail.com>
2024-12-06 18:07:36 +0200

    stage

--- src/modules/shared/components/Pagination/Pagination.tsx
+++ src/modules/shared/components/Pagination/Pagination.tsx
@@ -14,7 +14,7 @@ interface paginationInfo {
 }
 
 const Pagination: React.FC<paginationInfo> = ({
-  totalNumberOfPages,
+  totalNumberOfPages, 
   paginatedListFunction,
   pageNumber,
   numOfRecords,
